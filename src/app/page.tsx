616e6c9f71141b0490ef4ec9384cf2c336d03d5c
ayechan <ayechan.sant@gmail.com>
2024-06-07 12:13:03 -0400

    reading md function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
-import Image from "next/image";
+import fs from "fs";
 
-export default function Home() {
-  return <div>Hello</div>;
-}
+const getSectionMetaData = () => {
+  const folder = "sections/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) =>
+    file.endsWith(".md")
+  );
+  const slugs = markdownPosts.map((file) =>
+    file.replace(".md", "")
+  );
+  return slugs;
+};
+
+const Home = () => {
+  const sectionMetaData = getSectionMetaData();
+  const sectionPreviews = sectionMetaData.map((slug) => (
+    <div>
+      <h2>{slug}</h2>
+    </div>
+  ));
+  return (
+    <div>
+      Hello
+      <div>{sectionPreviews}</div>
+    </div>
+  );
+};
+
+export default Home;
