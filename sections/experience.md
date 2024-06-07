bd79cee287498ef6bab93fe3d82ebf6c2745c09a
ayechan <ayechan.sant@gmail.com>
2024-06-07 12:04:30 -0400

    sections

---
+++ sections/experience.md
@@ -0,0 +1,1 @@
+Here are my experiences that make me a good employee
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {head}
+        {header}
         {children}
         {footer}
       </body>
