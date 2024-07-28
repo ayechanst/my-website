0b8a7b47e21a12c750b01d640fe30f9a1e615f33
ayechan <ayechan.sant@gmail.com>
2024-07-27 18:15:09 -0700

    links operational

--- components/QuickLinks.tsx
+++ components/QuickLinks.tsx
@@ -3,57 +3,42 @@ const QuickLinks = () => {
     <>
       <ul className="menu menu-horizontal bg-base-200 rounded-box mt-6">
         <li>
-          <a className="tooltip" data-tip="Home">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
+          <a
+            className="tooltip"
+            data-tip="YouTube"
+            href="https://www.youtube.com/channel/UCoZ6nri-YUpNPy5h6zTAoJA"
+          >
+            <img
+              src="/youtube.svg"
+              alt="Home Icon"
               className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-              />
-            </svg>
+            />
           </a>
         </li>
         <li>
-          <a className="tooltip" data-tip="Details">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
+          <a
+            className="tooltip"
+            data-tip="Github"
+            href="https://github.com/ayechanst?ocid=AIDcmmc3fhtaow_SEM_3a7855d57351117d9c41364ee276a10e:G:s"
+          >
+            <img
+              src="/github.svg"
+              alt="Home Icon"
               className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+            />
           </a>
         </li>
         <li>
-          <a className="tooltip" data-tip="Stats">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
+          <a
+            className="tooltip"
+            data-tip="Medium"
+            href="https://medium.com/@ayechansant"
+          >
+            <img
+              src="/medium.svg"
+              alt="Home Icon"
               className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-              />
-            </svg>
+            />
           </a>
         </li>
       </ul>
