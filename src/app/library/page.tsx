dbf44845c132a05fda08d78da8f58c260a742d12
ayechan <ayechan.sant@gmail.com>
2024-07-27 19:36:06 -0700

    library organization

---
+++ src/app/library/page.tsx
@@ -0,0 +1,65 @@
+const Library = () => {
+  const bookArray = [
+    {
+      title:
+        "The Red Queen, Sex and the Evolution of Human Nature",
+      author: "Matt Ridley",
+    },
+    {
+      title:
+        "The Right to Sex, Feminism in the Twenty-First Century",
+      author: "Amia Srinivasan",
+    },
+    {
+      title: "King Warrior Magician Lover",
+      author: "Robert Moore & Douglas Gillette",
+    },
+    {
+      title: "The History of Money",
+      author: "Jack Weatherford",
+      class: "Social Science, finance, money (HG231.W4)",
+    },
+    {
+      title:
+        "Other Minds: The Octopus, the Sea, and the Deep Origins of Consciousness",
+      author: "Peter-Godfrey-Smith",
+    },
+    {
+      title: "Children of Time",
+      author: "Adrian Tchaikovsky",
+    },
+    {
+      title: "Genghis Khan",
+      author: "Jack Weatherford",
+    },
+    {
+      title:
+        "The Conspiracy Against the Human Race: A Contrivance of Horror",
+      author: "Thomas Ligotti",
+      class:
+        "Language and Literature, Literature(general), Theory . Philosophy . Esthetics (PN56)",
+    },
+  ];
+
+  return (
+    <>
+      <div>
+        <h3>Books</h3>
+        <div>
+          {bookArray.map((book) => (
+            <div key={`${book.author} + ${book.title}`}>
+              <div className="flex">
+                <div>{book.author} </div>
+                <div className="italic text-accent">
+                  {book.title}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Library;
--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -1,17 +1,14 @@
+import Link from "next/link";
+
 const PersonalPage = () => {
   return (
     <>
-      <div className="chat chat-start">
-        <div className="chat-bubble">
-          It's over Anakin,
-          <br />I have the high ground.
-        </div>
-      </div>
-      <div className="chat chat-end">
-        <div className="chat-bubble">
-          You underestimate my power!
-        </div>
-      </div>
+      <Link href="/library">Library</Link>
+      <p>Essays</p>
+      <p>Advice</p>
+      <p>My Story</p>
+      <p>Video Games</p>
+      <p>Life Philosophy</p>
     </>
   );
 };
